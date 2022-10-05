7e37aac8be189d860d8fdf50511db1c148d9100a
JeronimoHoulin <jeronimo.houlin@gmail.com>
2022-10-05 10:24:51 -0300

    browsing collection

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+#Connecting to ENV file
+load_dotenv()
+DATABASE_CONNECTION = os.getenv('DATABASE_CONNECTION')
+
+cluster = MongoClient(DATABASE_CONNECTION)
+
+db = cluster["test"]
+
+collection = db["users"]
 
 
-#cluster = MongoClient("")
+results = collection.find({"username":"jero"})
+
+#print(results)  #PyMongo cursos object!
+
+for result in results:
+    print(result)
